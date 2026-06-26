6ed1a9b238866855bc43647a07ee390b28275523
unknown <B11213119@yuntech.edu.tw>
2026-06-26 12:34:22 +0800

    改settings

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -74,9 +74,13 @@ WSGI_APPLICATION = "mysite.wsgi.application"
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'smart_pot_db',
+        'USER': 'group5_admin',
+        'PASSWORD': 'admin_password_555',
+        'HOST': 'yuntech_mysql',   # ⚠️ 你的 docker service name
+        'PORT': '3306',
     }
 }
 
